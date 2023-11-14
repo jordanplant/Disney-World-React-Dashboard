d4352c43342a24a3dcdd66b29d52edb9e023de46
Jordan Plant <jordanplant94@gmail.com>
2023-11-14 21:19:39 +0000

    is this going to work?

--- src/Components/SnacksList.jsx
+++ src/Components/SnacksList.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import Snacks from "./Snacks";
-import Styles from "./SnacksList.module.css"; // Import the CSS module
+import Styles from "./SnacksList.module.css";
 
 const SnacksList = () => {
   const [input, setInput] = useState("");
